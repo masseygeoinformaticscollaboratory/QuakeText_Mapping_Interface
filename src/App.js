9b215525b1b5dc59650cb9b350efa7abe063545c
Lydia <pricelydia776@gmail.com>
2022-12-08 08:47:58 +1300

    Tidied

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import Map from './Map';
 import './App.css';
 
 function App() {
-  return (
-      <div className="App">
-        <Map/>
-      </div>
-  );
+    return (
+        <div className="App">
+            <Map/>
+        </div>
+    );
 }
 
 export default App;
