849096eacdb245de49ce2ff58ec917198ef6d156
Lydia <pricelydia776@gmail.com>
2022-12-21 13:17:42 +1300

    Change File Structure

---
+++ Frontend/src/App.js
@@ -0,0 +1,21 @@
+import Map from "./MapComponents/BaseMap";
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="header">
+                <h1>Quake Text</h1>
+            </div>
+            <div className="map">
+                <Map/>
+            </div>
+
+
+        </div>
+    );
+}
+
+
+
+export default App;
