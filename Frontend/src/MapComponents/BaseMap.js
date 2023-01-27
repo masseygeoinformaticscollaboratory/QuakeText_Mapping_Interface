5df86a9793241117b7818d9d54b390bfea92af0f
Lydia <pricelydia776@gmail.com>
2023-01-27 14:53:12 +1300

    Geoserver finally working!

--- Frontend/src/MapComponents/BaseMap.js
+++ Frontend/src/MapComponents/BaseMap.js
@@ -1,30 +1,68 @@
 import './MapStyle.css';
 import React, {useEffect, useRef, useState} from 'react';
 import 'ol/ol.css';
-import TileWMS from 'ol/source/TileWMS';
+//import TileWMS from 'ol/source/TileWMS';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import 'ol/ol.css';
 import OSM from 'ol/source/OSM';
 import {Tile as TileLayer} from 'ol/layer';
+import {ImageWMS} from "ol/source";
+import {Group} from "ol/layer";
+import {Image} from "ol/layer"
 
-
-const base = new TileLayer({
-    source: new OSM(),
+const base = new Group({
+    'title': 'Base maps',
+    layers: [
+        new TileLayer({
+            title: 'OSM',
+            type: 'base',
+            visible: true,
+            source: new OSM()
+        })
+    ]
 });
 
 
-const layer = new TileLayer({
-    extent: [-13884991, 2870341, -7455066, 6338219],
-    source: new TileWMS({
-        url: 'http://localhost:8080/geoserver/wms?service=WMS',
-        params: {'LAYERS': 'topp:states', 'TILED': true},
-        serverType: 'geoserver',
-        //crossOrigin: 'anonymous',
-        // Countries have transparency, so do not fade tiles:
-        transition: 0,
-    }),
-})
+const overlays = new Group({
+    'title': 'Overlays',
+    layers: [
+        new Image({
+            title: 'aus1',
+            visible: true,
+            // extent: [-180, -90, -180, 90],
+            source: new ImageWMS({
+                url: 'http://localhost:8080/geoserver/wms',
+                params: {'LAYERS': 'aussie:aus1'},
+                ratio: 1,
+                serverType: 'geoserver'
+            })
+        }),
+        new Image({
+            title: 'aus2',
+            visible: true,
+            // extent: [-180, -90, -180, 90],
+            source: new ImageWMS({
+                url: 'http://localhost:8080/geoserver/wms',
+                params: {'LAYERS': 'aussie:aus2'},
+                ratio: 1,
+                serverType: 'geoserver'
+            })
+        }),
+        new Image({
+            title: 'aus3',
+            visible: true,
+            // extent: [-180, -90, -180, 90],
+            source: new ImageWMS({
+                url: 'http://localhost:8080/geoserver/wms',
+                params: {'LAYERS': 'aussie:aus3'},
+                ratio: 1,
+                serverType: 'geoserver'
+            })
+        })
+
+    ]
+});
 
 
 function BaseMap() {
@@ -36,9 +74,10 @@ function BaseMap() {
     useEffect(() => {
         const initialMap = new Map({
             target: mapElement.current,
-            layers: [base, layer],
+            layers: [base, overlays],
             view: new View({
-                center: [0, 0],
+                projection: 'EPSG:4326',
+                center: [78.0, 23.0],
                 zoom: 5,
                 minZoom: 2,
                 maxZoom: 18,
