6ed7977504e439096da01730de73dafcf26f3733
Lydia Price <pricelydia776@gmail.com>
2023-08-12 15:59:24 +1200

    More Data prepping

--- Place_Name_Disambiguation_Testing/prepare_quaketext_data.py
+++ Place_Name_Disambiguation_Testing/prepare_quaketext_data.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from pathlib import Path
 
+from geopy import GeoNames
+
 
 def read_data():
     print("Reading files...")
-    path = '../dataFiles'
+    path = '../Backend/dataFiles'
     files = Path(path).glob('*.json')
     dfs = list()
     for f in files:
@@ -30,21 +32,28 @@ def clean_df(data: pd.DataFrame):
         for row in get_database_rows(items, tweet):
             df = pd.concat([df, pd.DataFrame.from_records([row])])
         i += 1
-    df = df.drop_duplicates()
 
     df.dropna(inplace=True)
-    print(df)
+    df = df.drop_duplicates()
+
+    df.reset_index(drop=True, inplace=True)
     return df
 
 
 def get_database_rows(items, tweet):
     rows = []
+    geolocator = GeoNames(username='QuakeText')
 
     place_entities = get_relations(items, "place name")
 
     for place in place_entities:
         place_entity = tweet[place[0]:place[1]]
-        rows.append({'place name': place_entity, 'tweet text': tweet})
+        geonames_instances = geolocator.geocode(place, exactly_one=False)
+        for instance in geonames_instances:
+            print(instance.raw)
+            if instance.address.split(',') == place:
+                geonames_string = instance.raw.toponymName + ',' + ',' + instance.raw.countryName + ',' + instance.raw.countryCode + ',' + instance.raw.fcodeName
+                rows.append({'place name': place_entity, 'tweet text': tweet, "geonames id": instance.raw.geonameId, })
     return rows
 
 
@@ -55,3 +64,15 @@ def get_relations(list_of_lists, target):
             if item == target:
                 relations.append(sublist)
     return relations
+
+
+# data = read_data()
+# data.to_csv('quake_text_prepped_data.csv', index=False)
+
+geolocator = GeoNames(username='QuakeText')
+
+geonames_instances = geolocator.geocode('Pakistan', exactly_one=False)
+for instance in geonames_instances:
+    geonames_string = instance.raw.get('toponymName') + ', ' + instance.raw.get('countryName') + ', ' + instance.raw.get(
+        'countryCode') + ', ' + instance.raw.get('fcodeName')
+    print(geonames_string)
--- Place_Name_Disambiguation_Testing/test_geonames_instance.py
+++ Place_Name_Disambiguation_Testing/test_geonames_instance.py
@@ -8,9 +8,13 @@ from flair.data import Sentence
 def get_coordinates():
     geolocator = GeoNames(username='QuakeText')
 
-    place_name = "Balochistan"
+    place_name = "Pakistan"
     geonames_instances = geolocator.geocode(place_name, exactly_one=False)
-
+    print(geonames_instances)
     for instance in geonames_instances:
-        hello = instance.raw
-        print(instance.raw.get('fclName'))
+        if instance.address.split(',') == place_name:
+            #do some stuff here to add it to the data base
+
+
+
+get_coordinates()
