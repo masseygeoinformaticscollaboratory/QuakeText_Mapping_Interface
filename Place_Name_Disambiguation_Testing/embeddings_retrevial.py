0105276fac62408a476fb98b08ae9af18c309dbe
Lydia Price <pricelydia776@gmail.com>
2023-08-20 08:41:03 +1200

    Test Smaller data set

--- Place_Name_Disambiguation_Testing/embeddings_retrevial.py
+++ Place_Name_Disambiguation_Testing/embeddings_retrevial.py
@@ -9,7 +9,6 @@ import openai
 import config
 
 openai.api_key = config.api_key
-batch_size = 100
 model_name = "bert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
@@ -17,6 +16,7 @@ geolocator = GeoNames(username='QuakeText')
 
 
 def get_openai_embedding(text, model="text-embedding-ada-002"):
+    # 20s sleep added due to the restriction of only 3 calls per minute
     time.sleep(20)
     text = text.replace("\n", " ")
     return openai.Embedding.create(input=[text], model=model)['data'][0]['embedding']
@@ -54,14 +54,17 @@ def get_geonames_instance(place_entity):
 
 def run():
     start = time.time()
+
+    # Initialise data
     path = 'test.csv'
     text = 'text'
     location = 'location'
     data = pd.read_csv(path, low_memory=False)
-
+    '''
     data["open ai"] = np.nan
     data["geonames_lat_openai"] = np.nan
     data["geonames_lon_openai"] = np.nan
+    '''
     data["bert"] = np.nan
     data["geonames_lat_bert"] = np.nan
     data["geonames_lon_bert"] = np.nan
@@ -72,22 +75,7 @@ def run():
         for item in geonames_instances:
             geonames_strings.append(item.get("Geonames String"))
         if len(geonames_instances) > 0:
-            input_string_embedding_openai = [get_openai_embedding(x) for x in [row[text]]]
-            geo_names_embeddings_openai = [get_openai_embedding(x) for x in geonames_strings]
-            openai_cos_sim = calculate_cosine_similarity(input_string_embedding_openai, geo_names_embeddings_openai)
-
-            max_sim_openai = 0
-
-            for sim in openai_cos_sim[0]:
-                if max_sim_openai < sim:
-                    max_sim_openai = sim
-
-            data.at[index, "open ai"] = max_sim_openai
-            data.at[index, "geonames_lat_openai"] = geonames_instances[
-                np.argwhere(openai_cos_sim[0] == max_sim_openai)[0][0]].get('Geonames Latitude')
-            data.at[index, "geonames_lon_openai"] = geonames_instances[
-                np.argwhere(openai_cos_sim[0] == max_sim_openai)[0][0]].get('Geonames Longitude')
-
+            # For each row in the dataset retrieve the embeddings for the text and calculate the cos sim
             input_string_embeddings_bert = [get_bert_embedding(x) for x in [row[text]]]
             geo_names_embeddings_bert = [get_bert_embedding(x) for x in geonames_strings]
             bert_cos_sim = calculate_cosine_similarity(input_string_embeddings_bert, geo_names_embeddings_bert)
@@ -102,7 +90,7 @@ def run():
                 np.argwhere(bert_cos_sim[0] == max_sim_bert)[0][0]].get('Geonames Latitude')
             data.at[index, "geonames_lon_bert"] = geonames_instances[
                 np.argwhere(bert_cos_sim[0] == max_sim_bert)[0][0]].get('Geonames Longitude')
-
+    data = data.dropna(subset=["bert"])
     data.to_csv('testComp.csv', index=False)
     end = time.time()
     print(end - start)
@@ -110,4 +98,20 @@ def run():
 
 run()
 
+'''input_string_embedding_openai = [get_openai_embedding(x) for x in [row[text]]]
+            geo_names_embeddings_openai = [get_openai_embedding(x) for x in geonames_strings]
+            openai_cos_sim = calculate_cosine_similarity(input_string_embedding_openai, geo_names_embeddings_openai)
 
+            # Find the max cosine distance assuming this is < 1
+            max_sim_openai = 0
+
+            for sim in openai_cos_sim[0]:
+                if max_sim_openai < sim:
+                    max_sim_openai = sim
+
+            data.at[index, "open ai"] = max_sim_openai
+            data.at[index, "geonames_lat_openai"] = geonames_instances[
+                np.argwhere(openai_cos_sim[0] == max_sim_openai)[0][0]].get('Geonames Latitude')
+            data.at[index, "geonames_lon_openai"] = geonames_instances[
+                np.argwhere(openai_cos_sim[0] == max_sim_openai)[0][0]].get('Geonames Longitude')
+'''
