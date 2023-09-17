a046bf1d6785428f64af602c09604ea5dc8e0364
Lydia <pricelydia776@gmail.com>
2023-09-17 15:05:22 +1200

    Tidied multi threading

--- Place_Name_Disambiguation_Testing/mutliThreadedEmbeddings.py
+++ Place_Name_Disambiguation_Testing/mutliThreadedEmbeddings.py
@@ -1,25 +1,22 @@
 import time
+from concurrent.futures import ThreadPoolExecutor
+
 import numpy as np
 import pandas as pd
 from sqlalchemy import text
 from transformers import AutoTokenizer, AutoModel
 import torch
 from sklearn.metrics.pairwise import cosine_similarity
-import openai
 import concurrent.futures
+
+# Initialise data
+path = 'test.csv'
+tweet = 'text'
+location = 'location'
 
 model_name = "bert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
-
-
-def get_openai_embedding(text, model="text-embedding-ada-002"):
-    try:
-        text = text.replace("\n", " ")
-        return openai.Embedding.create(input=[text], model=model, timeout=2000)['data'][0]['embedding']
-    except openai.error.Timeout as e:
-        print(f"Request timed out for input text: {text} ", e)
-        return None
 
 
 def calculate_cosine_similarity(embeddings_1, embeddings_2):
@@ -36,53 +33,53 @@ def get_bert_embedding(sentence):
 def get_geonames_instance(place_entity, conn_engine):
     geonames_instances_lst = []
     place_entity_escaped = place_entity.replace("'", "")
-    query = text(f"SELECT * FROM geoname WHERE name ILIKE '% {place_entity_escaped} %' OR name ILIKE '{place_entity_escaped} %' OR name ILIKE '% {place_entity_escaped}' OR name ILIKE '{place_entity_escaped}'")
+    query = text(
+        f"SELECT * FROM geoname WHERE name ILIKE '% {place_entity_escaped} %' OR name ILIKE '{place_entity_escaped} %' OR name ILIKE '% {place_entity_escaped}' OR name ILIKE '{place_entity_escaped}'")
 
     matching_rows = conn_engine.execute(query)
 
     geonames_instances = pd.DataFrame(matching_rows.fetchall(), columns=matching_rows.keys())
 
     if not geonames_instances.empty:
-        for index, row in geonames_instances.iterrows():
-            country_name = None
-            continent = None
-            if row['country'] is not None:
-                query = text(f"SELECT * FROM countryinfo WHERE ISO = '{row['country']}'")
-                country = conn_engine.execute(query).fetchone()
-
-                if country is not None:
-                    country_name = country[1]
-                    continent = country[2]
-
-            fcode = None
-            if row['fcode'] is not None and row["fclass"] is not None:
-                code = row['fclass'] + '.' + row['fcode']
-                query = text(f"SELECT * FROM featurecodes WHERE code = '{code}'")
-                features = conn_engine.execute(query).fetchone()
-                if features is not None:
-                    fcode = features[1]
-
-            geonames_list = [row['name'], row['alternatenames'],
-                             country_name, continent, fcode]
-
-            cleaned_geonames_list = [item for item in geonames_list if item is not None]
-
-            geonames_instances_lst.append({"Geonames String": ', '.join(cleaned_geonames_list),
-                                           "Geonames ID": row['geonameid'],
-                                           "Geonames Latitude": row['latitude'],
-                                           "Geonames Longitude": row['longitude']})
+        num_threads = 16  # Adjust this based on your requirements
+        with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+            # Submit the tasks to the executor and retrieve the results
+            geonames_instances_lst = list(
+                executor.map(lambda x: process_geonames_row(conn_engine, x[1]), geonames_instances.iterrows()))
 
     return geonames_instances_lst
+
+
+def process_geonames_row(conn_engine, row):
+    country_name = None
+    continent = None
+    if row['country'] is not None:
+        query = text(f"SELECT * FROM countryinfo WHERE ISO = '{row['country']}'")
+        country = conn_engine.execute(query).fetchone()
+
+        if country is not None:
+            country_name = country[1]
+            continent = country[2]
+    fcode = None
+    if row['fcode'] is not None and row["fclass"] is not None:
+        code = row['fclass'] + '.' + row['fcode']
+        query = text(f"SELECT * FROM featurecodes WHERE code = '{code}'")
+        features = conn_engine.execute(query).fetchone()
+        if features is not None:
+            fcode = features[1]
+    geonames_list = [row['name'], row['alternatenames'],
+                     country_name, continent, fcode]
+    cleaned_geonames_list = [item for item in geonames_list if item is not None]
+    return {"Geonames String": ', '.join(cleaned_geonames_list),
+            "Geonames ID": row['geonameid'],
+            "Geonames Latitude": row['latitude'],
+            "Geonames Longitude": row['longitude']}
 
 
 def run(conn_engine):
     print("Beginning process")
     start = time.time()
 
-    # Initialise data
-    path = 'test.csv'
-    tweet = 'text'
-    location = 'location'
     data = pd.read_csv(path, low_memory=False)
 
     data["bert"] = np.nan
@@ -90,51 +87,45 @@ def run(conn_engine):
     data["geonames_lon_bert"] = np.nan
     data["geonames_id_bert"] = np.nan
 
-    count = 1
-    for index, row in data.iterrows():
-        start = time.time()
-        print(f"Tweet Number {count}: {row[tweet]}")
-        count += 1
+    with ThreadPoolExecutor(max_workers=16) as executor:
+        threads = [executor.submit(processRow, conn_engine, data, index, row) for index, row in data.iterrows()]
 
-        geonames_instances = get_geonames_instance(row[location], conn_engine)
-        geonames_strings = []
-
-        for item in geonames_instances:
-            geonames_strings.append(item.get("Geonames String"))
-        if len(geonames_instances) > 0:
-            print(f"Number of Geonames Instances: {len(geonames_instances)}")
-
-            input_string_embeddings_bert = [get_bert_embedding(x) for x in [row[tweet]]]
-
-            num_threads = len(geonames_strings)  # Adjust this based on your requirements
-
-            # Create a ThreadPoolExecutor with the specified number of threads
-            with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-                # Submit the tasks to the executor and retrieve the results
-                results = list(executor.map(get_bert_embedding, geonames_strings))
-
-            geo_names_embeddings_bert = results
-
-            bert_cos_sim = calculate_cosine_similarity(input_string_embeddings_bert, geo_names_embeddings_bert)
-            max_sim_bert = np.max(bert_cos_sim)
-
-            data.at[index, "bert"] = max_sim_bert
-            data.at[index, "geonames_lat_bert"] = geonames_instances[
-                np.argwhere(bert_cos_sim[0] == max_sim_bert)[0][0]].get('Geonames Latitude')
-            data.at[index, "geonames_lon_bert"] = geonames_instances[
-                np.argwhere(bert_cos_sim[0] == max_sim_bert)[0][0]].get('Geonames Longitude')
-            data.at[index, "geonames_id_bert"] = geonames_instances[
-                np.argwhere(bert_cos_sim[0] == max_sim_bert)[0][0]].get('Geonames ID')
-
-            end = time.time()
-            print(f"Time taken: {end - start}")
-
+    # Wait for all tasks to complete
+    concurrent.futures.wait(threads)
 
     data = data.dropna(subset=["bert"])
     data = data.astype({'geonames_id_bert': 'int'})
-    # data = data.astype({'geonames_id_openai': 'int'})
 
-    data.to_csv('TestSingleBert.csv', index=False)
+    data.to_csv('TestMultiBert.csv', index=False)
 
     end = time.time()
     print(f"Total time taken: {end - start}")
+
+
+def processRow(conn_engine, data, index, row):
+    geonames_instances = get_geonames_instance(row[location], conn_engine)
+    geonames_strings = []
+    for item in geonames_instances:
+        geonames_strings.append(item.get("Geonames String"))
+    if len(geonames_instances) > 0:
+        input_string_embeddings_bert = [get_bert_embedding(x) for x in [row[tweet]]]
+
+        num_threads = 16
+
+        # Create a ThreadPoolExecutor with the specified number of threads
+        with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+            # Submit the tasks to the executor and retrieve the results
+            results = list(executor.map(get_bert_embedding, geonames_strings))
+
+        geo_names_embeddings_bert = results
+
+        bert_cos_sim = calculate_cosine_similarity(input_string_embeddings_bert, geo_names_embeddings_bert)
+        max_sim_bert = np.max(bert_cos_sim)
+
+        data.at[index, "bert"] = max_sim_bert
+        data.at[index, "geonames_lat_bert"] = geonames_instances[
+            np.argwhere(bert_cos_sim[0] == max_sim_bert)[0][0]].get('Geonames Latitude')
+        data.at[index, "geonames_lon_bert"] = geonames_instances[
+            np.argwhere(bert_cos_sim[0] == max_sim_bert)[0][0]].get('Geonames Longitude')
+        data.at[index, "geonames_id_bert"] = geonames_instances[
+            np.argwhere(bert_cos_sim[0] == max_sim_bert)[0][0]].get('Geonames ID')
